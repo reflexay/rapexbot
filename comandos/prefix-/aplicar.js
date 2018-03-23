315c645486ee428fef457a009070c95224816e9b
reflexay <37716808+reflexay@users.noreply.github.com>
2018-03-23 19:14:20 -0300

    Update aplicar.js

--- comandos/prefix-/aplicar.js
+++ comandos/prefix-/aplicar.js
@@ -1,7 +1,8 @@
 exports.run = (client, message, args) => {
 message.channel.sendMessage({
     "embed": {
-      "description": "ㅤㅤㅤㅤㅤㅤㅤ<:golden:422427813392547862> **FORMULÁRIOS** <:golden:422427813392547862>ㅤㅤㅤㅤㅤ\nㅤ\n**Trial Mod: [clique aqui](https://pastebin.com/4cUsy0qj)**\n**ModGC: [clique aqui](https://pastebin.com/v8ct9ivV)**\n**Helper: [clique aqui](https://pastebin.com/zuqQtWs0)**",
+      "title": "Formulario para aplicação.",
+      "description": "ㅤㅤㅤㅤㅤㅤㅤ<:golden:422427813392547862> **Formularios** <:golden:422427813392547862>ㅤㅤㅤㅤㅤ\nㅤ\n**Trial Moderador: [Clique aqui](https://pastebin.com/4cUsy0qj)**\n**ModeradorGC: [Clique aqui](https://pastebin.com/v8ct9ivV)**\n**Helper: [Clique aqui](https://pastebin.com/zuqQtWs0)**",
       "color": 10223807,
       "thumbnail": {
         "url": "https://i.imgur.com/yhYiflU.png"
