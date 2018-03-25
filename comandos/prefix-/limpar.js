3eba4bada8e49113cf0d4cdf2e2fe6de54bb5270
reflexay <37716808+reflexay@users.noreply.github.com>
2018-03-24 22:20:16 -0300

    Update limpar.js

--- comandos/prefix-/limpar.js
+++ comandos/prefix-/limpar.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
   if(!message.member.roles.some(r=>["Dono 👑" , "Staff Manager 🔧" , "Social Manager🔧" ,  "ModGC 💣" , "YT+ 📷" , "Admin 🔪" , "Gerente 🔧" , "Moderador 🎈" , "Trial ❌"].includes(r.name)) ) return message.reply("Desculpe, apenas meus gerenciadores podem usar esse comando");
   let reason = args.slice(0).join(' ');
   if (reason.length < 1) return message.reply('**Diga a quantidade de mensagens que devo apagar!**');
-message.channel.bulkDelete(`${args[0]}`)
+message.channel.bulkDelete(`${args[0]} +1`)
   setTimeout(function() {
       message.channel.sendMessage(`**:wastebasket: ${args[0]} mensagens foram apagadas!**`).then((value) => {
           setTimeout(() => {
