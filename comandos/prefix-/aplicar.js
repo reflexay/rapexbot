678eb973c25b9d40b6e98182e5866c6ff5b4f8a9
reflexay <37716808+reflexay@users.noreply.github.com>
2018-03-25 19:34:08 -0300

    Update aplicar.js

--- comandos/prefix-/aplicar.js
+++ comandos/prefix-/aplicar.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 message.channel.sendMessage({
     "embed": {
       "title": "Aplique-se para a staff!",
-      "description": "ㅤㅤㅤㅤㅤㅤㅤ\n<:golden:422427813392547862> **Formularios** <:golden:422427813392547862>ㅤㅤㅤㅤㅤ\nㅤ\n**Trial Moderador: [Clique aqui](https://pastebin.com/4cUsy0qj)**\n**ModeradorGC: [Clique aqui](https://pastebin.com/v8ct9ivV)**\n**Helper: [Clique aqui](https://pastebin.com/zuqQtWs0)**\n\n**HRC: [Clique aqui](https://pastebin.com/ndUaM8dQ)\n\nQuer ser builder? mande seus ultimos 3 trabalhos para o @Reflexay#5071\nQuer ser designer? Mande seus ultimos 3 trabalhos para o @Reflexay#5071 \n\n**Requisitos para YT**\nYT: 2000 Subs + Feedback bom\nPro: 1000 Subs + Feedback bom\n\nCaso queira solicitar alguma outra tag, falar com @Reflexay#5071 .",
+      "description": "ㅤㅤㅤㅤㅤㅤㅤ\n<:golden:422427813392547862> **Formularios** <:golden:422427813392547862>ㅤㅤㅤㅤㅤ\nㅤ\n**Trial Moderador: [Clique aqui](https://pastebin.com/4cUsy0qj)**\n**ModeradorGC: [Clique aqui](https://pastebin.com/v8ct9ivV)**\n**Helper: [Clique aqui](https://pastebin.com/zuqQtWs0)**\n\n**HRC: [Clique aqui](https://pastebin.com/ndUaM8dQ)**\n\nQuer ser builder? mande seus ultimos 3 trabalhos para o @Reflexay#5071\nQuer ser designer? Mande seus ultimos 3 trabalhos para o @Reflexay#5071 \n\n**Requisitos para YT**\nYT: 2000 Subs + Feedback bom\nPro: 1000 Subs + Feedback bom\n\nCaso queira solicitar alguma outra tag, falar com @Reflexay#5071 .",
       "color": 10223807,
       "thumbnail": {
         "url": "https://i.imgur.com/yhYiflU.png"
